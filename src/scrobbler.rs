4620dfc99c5ba18c38f61c081d569a9ac6620590
David Futcher <bobbo@ubuntu.com>
2017-02-22 19:33:07 +0000

    Implement scrobble API call

--- src/scrobbler.rs
+++ src/scrobbler.rs
@@ -1,6 +1,7 @@
 use client::LastFmClient;
 
 use std::collections::HashMap;
+use std::time::UNIX_EPOCH;
 
 pub struct Scrobbler {
     client: LastFmClient
@@ -32,8 +33,19 @@ impl Scrobbler {
         }
     }
 
-    pub fn send_scrobble() -> Result<(), &'static str> {
-        Err("Not implemented")
+    pub fn scrobble(&self, name: String, artist: String) -> Result<(), String> {
+        let mut params = HashMap::new();
+        params.insert("track", name);
+        params.insert("artist", artist);
+        params.insert("timestamp", format!("{}", UNIX_EPOCH.elapsed().unwrap().as_secs()));
+
+        match self.client.send_authenticated_request("track.scrobble", &params) {
+            Ok(body) => {
+                println!("Body: {}", body);
+                Ok(())
+            },
+            Err(msg) => Err(msg)
+        }
     }
 
 }
