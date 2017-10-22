e7fef36b9772a158c77dfed986d84f47b6d9b419
David Futcher <david@futcher.io>
2017-10-22 10:46:54 +0100

    Improve example readability with a static scrobble macro

--- examples/example.rs
+++ examples/example.rs
@@ -6,6 +6,12 @@ use rustfm_scrobble::{Scrobbler, Scrobble};
 // scrobbling.
 // Replace credential values with your own to test.
 
+macro_rules! scrobble {
+    ($artist:expr, $track:expr, $album:expr) => (
+        Scrobble::new($artist.to_string(), $track.to_string(), $album.to_string())
+    )
+}
+
 fn main() {
     let api_key = "{{api_key}}".to_string();
     let api_secret = "{{api_secret}}".to_string();
@@ -19,22 +25,19 @@ fn main() {
         Err(e) => { println!("{}", e); }
     };
 
-    let artist = "Los Campesinos!".to_string();
-    let album = "No Blues".to_string();
-
-    let track_one = Scrobble::new("As Lucerne / The Low".to_string(), artist.clone(), album.clone());
+    let track_one = scrobble!("Los Campesinos!", "As Lucerne / The Low", "No Blues");
     match scrobbler.now_playing(track_one) {
         Ok(_) => { println!("Sent now playing! "); }
         Err(e) => { println!("{}", e); }
     }
 
-    let track_two = Scrobble::new("The Time Before the Last".to_string(), artist.clone(), album.clone());
+    let track_two = scrobble!("Los Campesinos!", "The Time Before the Last", "No Blues");
     match scrobbler.scrobble(track_two) {
         Ok(_) => { println!("Sent scrobble!"); }
         Err(e) => { println!("{}", e); }
     }
 
-    let track_three = Scrobble::new("Selling Rope".to_string(), artist.clone(), album.clone());
+    let track_three = scrobble!("Los Campesinos!", "Selling Rope", "No Blues");
     match scrobbler.now_playing(track_three) {
         Ok(_) => { println!("Sent now playing! "); }
         Err(e) => { println!("{}", e); }
